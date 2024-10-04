3fb9d4572b67a39339eb7dae6ed2f3fd7ba49439
lavrana <130138357+lavrana@users.noreply.github.com>
2024-10-04 19:49:21 +0530

    fix

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/cn-django-test
-sudo docker-compose down
 sudo docker-compose up -d --build
---
+++ stop_docker.sh
@@ -0,0 +1,2 @@
+cd /home/ec2-user/cn-django-test
+sudo docker-compose down
